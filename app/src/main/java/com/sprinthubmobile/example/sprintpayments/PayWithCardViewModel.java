6c04cf92dcdeb64633dd9f227b1c8cee0948c38c
odifek <kennedy998@gmail.com>
2019-03-13 11:55:07 +0100

    create card entry layout in xml

---
+++ app/src/main/java/com/sprinthubmobile/example/sprintpayments/PayWithCardViewModel.java
@@ -0,0 +1,7 @@
+package com.sprinthubmobile.example.sprintpayments;
+
+import androidx.lifecycle.ViewModel;
+
+public class PayWithCardViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
